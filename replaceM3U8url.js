149d75c06250101de2af4bec40d9c00d4b40e8b1
BIGSING <pushebp@163.com>
2022-08-10 20:26:11 +0800

    1

---
+++ replaceM3U8url.js
@@ -0,0 +1,37 @@
+function hookUrl() {
+  Java.perform(function () {
+    var tryPlayVideoBean = Java.use("com.fcppkgslhz.yhvrvbuznt.bean.TryPlayVideoBean");
+	tryPlayVideoBean.setUrl.implementation = function(url){
+		var p1 = -1;
+		var p2 = -1;
+		var start = '';
+		var end = '';
+		var newUrl = '';
+		console.log("url: ", url);
+		
+		p1 = url.indexOf('start=');
+		if(p1==-1){
+			this.setUrl(url);
+			return;
+		}
+		p2 = url.indexOf('&', p1);
+		start = url.substring(p1, p2);
+		
+		p1 = url.indexOf('end=');
+		if(p1==-1){
+			this.setUrl(url);
+			return;
+		}
+		p2 = url.indexOf('&', p1);
+		end = url.substring(p1, p2);
+		
+		url = url.replace(start, 'start=0');
+		url = url.replace(end, 'end=99999');
+		console.log("new: ", url);
+        this.setUrl(newUrl);
+    }
+	
+  });
+}
+setImmediate(hookUrl());
+// frida -U com.fcppkgslhz.yhvrvbuznt -l replaceM3U8url.js
