3af8328a94b77b7005048736801a9e3a0cde8d7f
BIGSING <pushebp@163.com>
2022-08-30 21:41:09 +0800

    1

--- replaceM3U8url.js
+++ replaceM3U8url.js
@@ -1,18 +1,27 @@
 function hookUrl() {
   Java.perform(function () {
     var tryPlayVideoBean = Java.use("com.fcppkgslhz.yhvrvbuznt.bean.TryPlayVideoBean");
-	tryPlayVideoBean.setUrl.implementation = function(url){
+	console.log(tryPlayVideoBean);
+	tryPlayVideoBean.class.getDeclaredMethods().forEach(function (method) {
+      console.log(method);
+    });
+	
+
+	tryPlayVideoBean.getUrl.implementation = function(){
+		var url = this.getUrl();
+		console.log("url: ", url);
+		//console.log(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Throwable").$new()));//java打印堆栈
 		var p1 = -1;
 		var p2 = -1;
 		var start = '';
 		var end = '';
 		var newUrl = '';
-		console.log("url: ", url);
+		console.log("url: " + url);
 		
 		p1 = url.indexOf('start=');
 		if(p1==-1){
 			this.setUrl(url);
-			return;
+			return url;
 		}
 		p2 = url.indexOf('&', p1);
 		start = url.substring(p1, p2);
@@ -20,16 +29,17 @@ function hookUrl() {
 		p1 = url.indexOf('end=');
 		if(p1==-1){
 			this.setUrl(url);
-			return;
+			return url;
 		}
 		p2 = url.indexOf('&', p1);
 		end = url.substring(p1, p2);
 		
 		newUrl = url.replace(start, 'start=0');
-		newUrl = newUrl.replace(end, 'end=99999');
-		console.log("new: ", newUrl);
-        this.setUrl(newUrl);
-    }
+		newUrl = newUrl.replace(end, 'end=9');
+		console.log("new: " + newUrl);
+		
+		return newUrl;
+	}
 	
   });
 }
