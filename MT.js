8fb2b415095b65e47eef0f33b453a0edb9c23851
BIGSING <BIGSING@163.com>
2022-09-20 12:07:45 +0800

    1

--- MT.js
+++ MT.js
@@ -23,16 +23,20 @@ Java.perform(function(){
 			}
         }
     }); */
-		
-		
-	var dayin = false;
+	
+	// 常量定义
+	var PACKAGENAME = "com.zyzc.mt";
+	var SAVEDIR = "cache"
+	
+	
+	
 	var android_dlopen_ext = Module.findExportByName(null, "android_dlopen_ext");
 	if(android_dlopen_ext != null){
 		Interceptor.attach(android_dlopen_ext,{
 			onEnter: function(args){
 				var soName = args[0].readCString();
-				console.log("android_dlopen_ext: " + soName);
 				if(soName.indexOf("libgame.so") != -1){//libcocos2dlua.so
+					console.log("android_dlopen_ext: " + soName);
 					this.hook = true;
 				}
 			},
@@ -46,63 +50,210 @@ Java.perform(function(){
 		});
 	}
 	
-		
+// 字节序列保存到文件中
+function save2File(filename, byteArr){
+	console.log("save file to: " + filename);
+	var file = new File(filename, "w");
+	file.write(byteArr);
+	file.flush();
+	file.close();
+}
+
+// 输出显示十六进制数据
+function printDataHexStr(byteArr, size){
+	var len = size;
+	if(len==undefined){len=0x40;}
+	console.log(byteArr);
+
+/* 	var b = new Uint8Array(byteArr);
+	var str = "";
+
+	for(var i = 0; i < len; i++) {
+		str += (b[i].toString(16) + " ");
+	}
+	console.log(str); */
+}
 	
 function frida_Module() {
     Java.perform(function () {
 
         var process_Obj_Module_Arr = Process.enumerateModules();
         for(var i = 0; i < process_Obj_Module_Arr.length; i++) {
-            //if(process_Obj_Module_Arr[i].path.indexOf("hello")!=-1)
-            //{
+            if(process_Obj_Module_Arr[i].path.indexOf("libgame")!=-1) {
                 console.log("模块名称:",process_Obj_Module_Arr[i].name);
                 console.log("模块地址:",process_Obj_Module_Arr[i].base);
                 console.log("大小:",process_Obj_Module_Arr[i].size);
                 console.log("文件系统路径",process_Obj_Module_Arr[i].path);
-            //}
+            }
          }
     });
 }
-//frida_Module();
-dlopentodo();
 
 
 	function dlopentodo(){
 		console.log("hook start");
-		//加载lua文件函数 libcocos2dlua.so
-		var base = Module.findBaseAddress('libgame.so');
-		//if(base==null){ base = 0x04000000; }
-		var addr = Module.findExportByName(base, "luaL_loadbuffer");
+		
+		//getFileData
+		Interceptor.attach(Module.findExportByName("libgame.so" , "_ZN7cocos2d11CCFileUtils11getFileDataEPKcS2_Pmb"),{
+			onEnter:function (args){
+				var name = Memory.readCString(args[1]);
+				this.fileDescriptor = name;
+				this.sizePtr = ptr(args[3]);
+				if(name.indexOf("ea_autofight_list.lua")!=-1){
+					console.log("getFileData onEnter, name: " + name);
+				}
+			},
+			onLeave:function (retval){
+ 				if(this.fileDescriptor.indexOf("ea_autofight_list.lua")!=-1){
+					var name = this.fileDescriptor;
+					var size = Memory.readInt(this.sizePtr);
+					console.log("getFileData onLeave, name: " + name + " size: ", size);
+					//var buff = Memory.readCString(retval);
+					//console.log(buff);
+					printDataHexStr(Memory.readByteArray(retval, size));
+					
+					if(!name.endsWith(".lua")){name=name+".lua";}
+ 					var filename = "/data/user/0/" + PACKAGENAME + "/" + SAVEDIR + "/" + name.split("/").join(".");
+					save2File(filename, Memory.readByteArray(retval, size));
+				} 	
+			}			
+		});
+		
+		//luaL_loadbuffer 加载lua文件函数 libcocos2dlua.so
+		var addr = Module.findExportByName('libgame.so', "luaL_loadbuffer");
 		console.log("luaL_loadbuffer: " + addr);
 		if(addr!=null){
 			Interceptor.attach(addr,{
 				onEnter:function (args){
-	/* 			    this.fileout = "/data/local/tmp/frida/lua/" + Memory.readCString(args[3]).split("/").join(".");
-					console.log("read file from: " + this.fileout);
-					var tmp = Memory.readByteArray(args[1], args[2].toInt32());
-					var file = new File(this.fileout, "w");
-					file.write(tmp);
-					file.flush();
-					file.close();
-
-					console.log("lual_loadbuffer (" +Memory.readCString (args[3] ) +" ，" +Memory.readCString (args[1])+")"); */
-				},
-				onLeave:function (retval){
-					//console.log(retval)
+					var name = Memory.readCString(args[3]);
+					if(name=="ea_autofight_list"){
+						var buff = Memory.readCString(args[1]);
+						var size = args[2].toInt32();
+						//console.log("lual_loadbuffer, name: " + name + " size: ", size);
+						//console.log(buff);
+						var byteArr = Memory.readByteArray(args[1], size);
+						printDataHexStr(byteArr);
+						
+						if(!name.endsWith(".lua")){name=name+".lua";}
+ 						var filename = "/data/user/0/" + PACKAGENAME + "/" + SAVEDIR + "/" + name.split("/").join(".");
+						save2File(filename, byteArr);
+					}
 				}
 			});
 		}
 		
-/* 		//再贴个获取xxtea秘钥的 libcocos2dlua.so _Z13xxtea_decryptPhjS_jPj
-		Interceptor.attach(Module.findExportByName("libgame.so" , "xxtea_decode"),{
+/* 		
+		Interceptor.attach(Module.findExportByName("libgame.so" , "_ZN7cocos2d11CCLuaEngine13executeStringEPKc"),{
 			onEnter:function (args){
-				console.log(Memory.readUtf8String(args[2]));
-				console.log("\n");
+				console.log(' called from:\n' +Thread.backtrace(this.context, Backtracer.ACCURATE).map(DebugSymbol.fromAddress).join('\n') + '\n');
 			},
 			onLeave:function (retval){
 				//console.log(retval)
 			}
+		});
+		
+ 		Interceptor.attach(Module.findExportByName("libc.so" , "open"),{
+			onEnter:function (args){
+				var name = Memory.readUtf8String(args[0]);
+				if(name.indexOf(".lua")!=-1){
+					console.log(
+					  "open(" +
+					  "path=\"" + Memory.readUtf8String(args[0]) + "\"" +
+					  ", flag=" + args[1] +
+						")"
+					);
+					console.log('called from:\n' +Thread.backtrace(this.context, Backtracer.ACCURATE).map(DebugSymbol.fromAddress).join('\n') + '\n');	
+				}
+			}
+		});
+		
+		Interceptor.attach(Module.findExportByName("libgame.so" , "_ZN13CScriptSystem6DoFileEPKc"),{
+			onEnter:function (args){
+				var name = Memory.readCString(args[1]);
+				console.log("DoFile: " + name);
+				//console.log('called from:\n' +Thread.backtrace(this.context, Backtracer.ACCURATE).map(DebugSymbol.fromAddress).join('\n') + '\n');
+			}
+		});
+*/
+
+		
+		//再贴个获取xxtea秘钥的 libcocos2dlua.so _Z13xxtea_decryptPhjS_jPj
+		
+		Interceptor.attach(Module.findExportByName("libgame.so" , "_Z13getPackagekeyiPi"),{
+			onEnter:function (args){
+				console.log("getPackagekey onEnter");
+			},
+			onLeave:function (retval){
+				console.log("getPackagekey onLeave");
+			}
+		});
+		Interceptor.attach(Module.findExportByName("libgame.so" , "npk_read_encrypt"),{
+			onEnter:function (args){
+				console.log("npk_read_encrypt onEnter");
+			},
+			onLeave:function (retval){
+				console.log("npk_read_encrypt onLeave");
+			}
+		});
+		
+		Interceptor.attach(Module.findExportByName("libgame.so" , "tea_decode_buffer"),{
+			onEnter:function (args){
+				console.log("tea_decode_buffer");
+				var pKey = ptr(arg[2]);
+				console.log("tea_decode_buffer key: ", Memory.readUInt(pKey), Memory.readUInt(pKey+4),  Memory.readUInt(pKey+8),  Memory.readUInt(pKey+_12)); 
+			}
+		});
+		Interceptor.attach(Module.findExportByName("libgame.so" , "tea_decode"),{
+			onEnter:function (args){
+				console.log("tea_decode");
+			}
+		});
+		Interceptor.attach(Module.findExportByName("libgame.so" , "npk_read_encrypt"),{
+			onEnter:function (args){
+				console.log("npk_read_encrypt");
+				var pKey = ptr(arg[0]);
+				console.log("tea_decode_buffer key: ", Memory.readUInt(pKey), Memory.readUInt(pKey+4),  Memory.readUInt(pKey+8),  Memory.readUInt(pKey+_12)); 
+			}
+		});
+		
+/* 		Interceptor.attach(Module.findExportByName("libgame.so" , "xxtea_decode"),{
+			onEnter:function (args){
+				console.log("xxtea: xxtea_decode");// +Memory.readUtf8String(args[2])
+			}
 		}); */
+		Interceptor.attach(Module.findExportByName("libgame.so" , "xxtea_decode_byte"),{
+			onEnter:function (args){
+				console.log("xxtea: xxtea_decode_byte");
+			},
+			onLeave:function (retval){
+				//console.log(retval)
+			}
+		});
+		Interceptor.attach(Module.findExportByName("libgame.so" , "xxtea_decode_buffer"),{
+			onEnter:function (args){
+				console.log("xxtea: xxtea_decode_buffer");
+			},
+			onLeave:function (retval){
+				//console.log(retval)
+			}
+		});
+		
+/* 		Interceptor.attach(Module.findExportByName("libgame.so" , "xxtea_encode"),{
+			onEnter:function (args){
+				console.log("xxtea: xxtea_encode");
+			}
+		}); */
+		Interceptor.attach(Module.findExportByName("libgame.so" , "xxtea_encode_byte"),{
+			onEnter:function (args){
+				console.log("xxtea: xxtea_encode_byte");
+			}
+		});
+		Interceptor.attach(Module.findExportByName("libgame.so" , "xxtea_encode_buffer"),{
+			onEnter:function (args){
+				console.log("xxtea: xxtea_encode_buffer");
+			}
+		});
+		
 
 	}
 
