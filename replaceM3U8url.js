5fcc391f4e5ca0a5dc44de1acc6502076f014ec3
BIGSING <BIGSING@163.com>
2024-01-04 15:57:36 +0800

    1

--- replaceM3U8url.js
+++ replaceM3U8url.js
@@ -1,47 +1,30 @@
-function hookUrl() {
-  Java.perform(function () {
-    var tryPlayVideoBean = Java.use("com.fcppkgslhz.yhvrvbuznt.bean.TryPlayVideoBean");
-	console.log(tryPlayVideoBean);
-	tryPlayVideoBean.class.getDeclaredMethods().forEach(function (method) {
-      console.log(method);
-    });
-	
-
-	tryPlayVideoBean.getUrl.implementation = function(){
-		var url = this.getUrl();
-		console.log("url: ", url);
-		//console.log(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Throwable").$new()));//java打印堆栈
-		var p1 = -1;
-		var p2 = -1;
-		var start = '';
-		var end = '';
-		var newUrl = '';
-		console.log("url: " + url);
-		
-		p1 = url.indexOf('start=');
-		if(p1==-1){
-			this.setUrl(url);
-			return url;
+function hookURL() {
+	var URL = Java.use('java.net.URL');
+	URL.$init.overload('java.lang.String').implementation = function (url) {
+		if (url.indexOf('start=') > 0 && url.indexOf('end=') > 0) {
+			//console.log('URL：' + url);
+			var url2 = replaceUrlArgs(url, "start", 0);
+			url2 = replaceUrlArgs(url2, "end", 99999);
+			console.log('URL：' + url2);
+			this.$init(url2);
 		}
-		p2 = url.indexOf('&', p1);
-		start = url.substring(p1, p2);
-		
-		p1 = url.indexOf('end=');
-		if(p1==-1){
-			this.setUrl(url);
-			return url;
-		}
-		p2 = url.indexOf('&', p1);
-		end = url.substring(p1, p2);
-		
-		newUrl = url.replace(start, 'start=0');
-		newUrl = newUrl.replace(end, 'end=9');
-		console.log("new: " + newUrl);
-		
-		return newUrl;
 	}
-	
-  });
 }
-setImmediate(hookUrl());
-// frida -U com.fcppkgslhz.yhvrvbuznt -l replaceM3U8url.js
+
+function replaceUrlArgs(url, name, value) {
+	const re = new RegExp(name + '=[^&]*', 'gi')
+	return url.replace(re, name + '=' + value)
+}
+
+function replaceUrlArgs2(url, name, value) {
+	const re = new RegExp(`(?<=${name}=)[^&]*`, 'g');
+	return url.replace(re, value)
+}
+
+function hook() {
+	Java.perform(function () {
+		hookURL();
+	});
+}
+
+setImmediate(hook());
